9a1f04e2a4a1b209aabbb9ab905d0794bad6792e
xiaogao3 <1768617510@qq.com>
2020-01-14 16:33:23 +0800

    test

---
+++ hello.github/src/com/mage/hello/HelloGithub.java
@@ -0,0 +1,7 @@
+package com.mage.hello;
+
+public class HelloGithub {
+    public static void main(String[] args) {
+        System.out.println("I do it just for a test in commit operation");
+    }
+}
